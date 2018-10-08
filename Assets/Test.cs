216578bdacefcf7abe15746ac1628d1d20447bf8
Tarkest <33873988+Tarkest@users.noreply.github.com>
2018-10-08 19:40:08 +0300

    (Init)

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Test : MonoBehaviour {
+
+    public void NormalAd()
+    {
+        if(Advertisement.IsReady("video"))
+        {
+            Advertisement.Show("video");
+        }
+    }
+
+    public void RewardedAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            Advertisement.Show("rewardedVideo");
+        }
+    }
+}
